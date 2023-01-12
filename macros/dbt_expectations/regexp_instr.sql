ff8d9dacce2ed0596c06af33f85df4c2b14954b7
Owen <74567580+owenprough-sift@users.noreply.github.com>
2023-01-12 09:11:05 -0500

    Create regexp_instr.sql
    Compatibility shim for `dbt_expectations.expect_column_values_to_match_regex()`

---
+++ macros/dbt_expectations/regexp_instr.sql
@@ -0,0 +1,7 @@
+{# Athena does not have the "regexp_instr" function, but we can approximate it with "regexp_like" #}
+{#   Parameters `position`, `occurrence`, and `is_raw` are therefore ignored #}
+{# To match behavior of regexp_instr(), we need to return >0 when pattern is found and 0 when pattern is not found #}
+
+{% macro athena__regexp_instr(source_value, regexp, position, occurrence, is_raw) %}
+cast( regexp_like( {{ source_value }}, '{{ regexp }}' ) as int )
+{% endmacro %}
