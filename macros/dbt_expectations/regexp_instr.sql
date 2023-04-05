5fb39766342d9b2beb6d34b873d76b233b2a1006
Simon Llagone <simon.llagone@jobteaser.com>
2023-04-05 18:28:10 +0200

    fix: add flags arg to athena__regexp_instr macro in order to match dbt_expectations dispatch

--- macros/dbt_expectations/regexp_instr.sql
+++ macros/dbt_expectations/regexp_instr.sql
@@ -2,6 +2,6 @@
 {#   Parameters `position`, `occurrence`, and `is_raw` are therefore ignored #}
 {# To match behavior of regexp_instr(), we need to return >0 when pattern is found and 0 when pattern is not found #}
 
-{% macro athena__regexp_instr(source_value, regexp, position, occurrence, is_raw) %}
+{% macro athena__regexp_instr(source_value, regexp, position, occurrence, is_raw, flags) %}
 cast( regexp_like( cast( {{ source_value }} as varchar ), '{{ regexp }}' ) as int )
 {% endmacro %}
